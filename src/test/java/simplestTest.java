4d8d6dc4a152f7898e396b4ad3b9633886c16b02
robin <robin.vangroesen@digib.com>
2020-05-26 11:42:09 +0200

    Initial framework commit

---
+++ src/test/java/simplestTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class simplestTest {
+
+    @Test
+    void should_always_succeed() {
+        assertTrue(true);
+    }
+}
